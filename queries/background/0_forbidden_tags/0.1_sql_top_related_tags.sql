fe42329fd9d3c261564cf40ad932adc1e89c0740
J-JJJJJJ <26305672+J-JJJJJJ@users.noreply.github.com>
2022-05-16 06:38:04 +0200

    Update 0.1_sql_top_related_tags.sql

--- queries/background/0_forbidden_tags/0.1_sql_top_related_tags.sql
+++ queries/background/0_forbidden_tags/0.1_sql_top_related_tags.sql
@@ -1,12 +1,14 @@
+-- 1a explore tags on 
 -- adapted from https://meta.stackexchange.com/a/293208
-With PostTags as (SELECT
+With SQLtaggedQuestions as
+(select * from Posts where Tags like '%<sql>%'),
+PostTags as (SELECT
   Id,
   value TagName
 FROM
-  SQLquestions 
+  SQLtaggedQuestions 
   CROSS APPLY STRING_SPLIT(REPLACE(REPLACE(REPLACE(Tags,'><',','),'<',''),'>',''),',')
   )
 
   select top 500 count(Id) frequency, TagName, '1' as excludeTag from PostTags 
-  where TagName not like '%sql%'
   group by TagName order by count(*) DESC
