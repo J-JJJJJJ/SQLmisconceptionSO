1e687406d440bceaf4e7f4d2fe6d82160d90e536
J-JJJJJJ <26305672+J-JJJJJJ@users.noreply.github.com>
2022-05-17 06:16:24 +0200

    Update 0.2_tag_to_sql.r

--- queries/background/0_forbidden_tags/0.2_tag_to_sql.r
+++ queries/background/0_forbidden_tags/0.2_tag_to_sql.r
@@ -7,15 +7,20 @@ library(dplyr)
 library(data.table)
 
 
+# read results of 0.1_sql_top_related_tags.sql after manually classifying which tags should be excluded 
 tags<-fread('excludeTags.csv', header = F) %>% arrange(V2)
 fwrite(tags, 'excludeTags.csv', col.names = F)
 
+
+# split tags based on the assigned classification
 tags_exclude<-tags %>% dplyr::filter(V3==1) %>% arrange(V2)
 tags_exclude<-tags_exclude$V2
 
 tags_include<-tags %>% dplyr::filter(V3==0) %>% arrange(V2)
 tags_include<-tags_include$V2
 
+
+# export included and excluded tags to files
 fileConn<-file("included_tags.txt")
 writeLines(paste0(tags_include, collapse = ", "), fileConn)
 close(fileConn)
@@ -26,11 +31,9 @@ writeLines(paste0(tags_exclude, collapse = ", "), fileConn)
 close(fileConn)
 
 
+
+# create SQL code for excluding questions based on tags for use in 1_add_sql_to_table.sql
 produce_sql<-T
-
-
-
-
 
 if(produce_sql){
 lines<-character(0)
